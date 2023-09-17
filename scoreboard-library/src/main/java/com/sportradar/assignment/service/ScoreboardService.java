f23ec7f4a895f91c544e62d80cfa44a615bfb626
geekyneelkant <99695307+geekyneelkant@users.noreply.github.com>
2023-09-17 20:30:25 +0200

    Added more tests and further exceptions
    - Exception handling with custom messages.

--- scoreboard-library/src/main/java/com/sportradar/assignment/service/ScoreboardService.java
+++ scoreboard-library/src/main/java/com/sportradar/assignment/service/ScoreboardService.java
@@ -3,12 +3,17 @@ package com.sportradar.assignment.service;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Predicate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import com.sportradar.assignment.domains.Match;
 
 public class ScoreboardService {
+
+	private static final Logger logger = Logger.getLogger(ScoreboardService.class.getName());
 
 	List<Match> liveMatches;
 
@@ -25,12 +30,14 @@ public class ScoreboardService {
 	 * Team Names wouldn't have super extra long names like exceeding the length of
 	 * the string variable.
 	 */
-	public void startMatch(String home, String away) {
-		if (!isTeamNamesNullOrBlank(home, away)) {
-			if (!hasLiveMatch(home, away, liveMatches)) {
-				liveMatches.add(new Match(home, away));
+	public void startMatch(String home, String away) throws IllegalArgumentException{
+			if (!isTeamNamesNullOrBlank(home, away)) {
+				if (!hasLiveMatch(home, away, liveMatches)) {
+					liveMatches.add(new Match(home, away));
+				}
+			} else {
+				throw new IllegalArgumentException("Cannot start the match with Null Or Blank Team Names");
 			}
-		}
 	}
 
 	/*
@@ -43,8 +50,14 @@ public class ScoreboardService {
 	 * the string variable.
 	 */
 	public void finishMatch(String home, String away) {
-		if (!isTeamNamesNullOrBlank(home, away)) {
-			liveMatches.removeIf(match -> checkLiveMatch(home, away, match));
+		try {
+			if (!isTeamNamesNullOrBlank(home, away)) {
+				liveMatches.removeIf(match -> checkLiveMatch(home, away, match));
+			} else {
+				throw new IllegalArgumentException("Cannot finish the match with Null Or Blank Team Names");
+			}
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "An error occurred while finishing the match : " + e.getMessage(), e);
 		}
 	}
 
@@ -52,18 +65,25 @@ public class ScoreboardService {
 	 * Updates score of provided home team score and away team score in the live
 	 * score board.
 	 * 
-	 * The following are the assumptions - Team Names would contain country name but
+	 * The following are the assumptions - Team Names would contain country names but
 	 * not special characters because this is World Cup ScoreBoard Application -
 	 * Team Names wouldn't have super extra long names like exceeding the length of
 	 * the string variable.
 	 */
 	public void updateScore(String home, String away, int homeScore, int awayScore) {
-		if (!isTeamNamesNullOrBlank(home, away)) {
-			liveMatches.forEach(liveMatch -> {
-				if (checkLiveMatch(home, away, liveMatch)) {
-					liveMatch.updateScore(homeScore, awayScore);
-				}
-			});
+		try {
+			if (!isTeamNamesNullOrBlank(home, away) && !isNegativeScore(homeScore, awayScore)) {
+				liveMatches.forEach(liveMatch -> {
+					if (checkLiveMatch(home, away, liveMatch)) {
+						liveMatch.updateScore(homeScore, awayScore);
+					}
+				});
+			} else {
+				throw new IllegalArgumentException(
+						"Cannot updateScore the match score with Null " + "Or Blank Team Names or Negative Scores");
+			}
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "An error occurred while updating the scores : " + e.getMessage(), e);
 		}
 	}
 
@@ -73,14 +93,20 @@ public class ScoreboardService {
 	 * recently started match in the score board.
 	 */
 	public List<Match> getLiveMatchesSummary() {
-		List<Match> sortLiveMatches = this.getLiveMatches().stream()
-				.sorted(Comparator.comparing((Match match) -> match.getStartTime()).reversed())
-		        .sorted(Comparator.comparingInt((Match match) -> match.getTotalScore()).reversed())
-				.collect(Collectors.toList());
-		this.displayLiveMatchesSummary(sortLiveMatches);
-		return sortLiveMatches;
+		List<Match> sortLiveMatches = null;
+		try {
+			sortLiveMatches = this.getLiveMatches().stream()
+					.sorted(Comparator.comparing((Match match) -> match.getStartTime()).reversed())
+					.sorted(Comparator.comparingInt((Match match) -> match.getTotalScore()).reversed())
+					.collect(Collectors.toList());
+			return sortLiveMatches;
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "An error occurred while getting the Summary of Live Matches : " + e.getMessage(),
+					e);
+			return sortLiveMatches;
+		}
 	}
-	
+
 	public void displayLiveMatchesSummary(List<Match> liveMatches) {
 		System.out.println("Scoreboard summary of Live Matches :");
 		liveMatches.forEach(match -> System.out.println(match));
@@ -99,12 +125,15 @@ public class ScoreboardService {
 	}
 
 	public static boolean checkLiveMatch(String home, String away, Match liveMatch) {
-		return liveMatch.getHomeTeam().equalsIgnoreCase(home) 
-				|| liveMatch.getHomeTeam().equalsIgnoreCase(away)
+		return liveMatch.getHomeTeam().equalsIgnoreCase(home) || liveMatch.getHomeTeam().equalsIgnoreCase(away)
 				|| liveMatch.getAwayTeam().equalsIgnoreCase(home) || liveMatch.getAwayTeam().equalsIgnoreCase(away);
 	}
 
 	private static boolean isTeamNamesNullOrBlank(String home, String away) {
-		return home == null || home.isBlank() || away == null || away.isBlank();
+		return Objects.isNull(home) || Objects.isNull(away);
+	}
+
+	public boolean isNegativeScore(int homeScore, int awayScore) {
+		return homeScore < 0 || awayScore < 0;
 	}
 }
--- scoreboard-library/src/test/java/com/sportradar/assignment/service/ScoreboardServiceTest.java
+++ scoreboard-library/src/test/java/com/sportradar/assignment/service/ScoreboardServiceTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -56,21 +57,24 @@ public class ScoreboardServiceTest {
 	}
 
 	@Test
-	public void startMatch_AddNullAsTeam_ShouldNotCreateNewMatch() {
-		scoreboardService.startMatch(null, null);
+	public void startMatch_AddNullAsTeam_ShouldNotCreateNewMatchThrowsException() {
+		
 		assertEquals(0, scoreboardService.getLiveMatches().size());
+	IllegalArgumentException thrown = 	assertThrows(IllegalArgumentException.class, () -> scoreboardService.startMatch(null, null));
+	Assertions.assertEquals("Cannot start the match with Null Or Blank Team Names", thrown);
 	}
 
 	@Test
-	public void startMatch_AddEmptyAsTeamNames_ShouldNotCreateNewMatch() {
-		scoreboardService.startMatch("", "");
+	public void startMatch_AddEmptyAsTeamNames_ShouldThrowExceptionNotCreateNewMatch() {
 		assertEquals(0, scoreboardService.getLiveMatches().size());
+		assertThrows(IllegalArgumentException.class, () -> scoreboardService.startMatch("", ""));
 	}
 
 	@Test
-	public void startMatch_AddBlankAsTeamNames_ShouldNotCreateNewMatch() {
-		scoreboardService.startMatch(" ", " ");
+	public void startMatch_AddBlankAsTeamNames_ShouldThrowExceptionNotCreateNewMatch() {
+		
 		assertEquals(0, scoreboardService.getLiveMatches().size());
+		assertThrows(IllegalArgumentException.class, () -> scoreboardService.startMatch(" ", " "));
 	}
 
 	/* Tests for finishMatch() */
@@ -99,19 +103,22 @@ public class ScoreboardServiceTest {
 	}
 
 	@Test
-	public void finishMatch_removeWithEmptyParameters_ShouldNotThrowExceptionAndDoNotRemoveTheMatch() {
+	public void finishMatch_removeWithEmptyParameters_ShouldThrowExceptionAndDoNotRemoveTheMatch() {
 		scoreboardService.startMatch("Brazil", "Argentina");
 		scoreboardService.startMatch("Spain", "Portugal");
 		scoreboardService.finishMatch("", "");
 		assertEquals(2, scoreboardService.getLiveMatches().size());
+		assertThrows(IllegalArgumentException.class, () -> scoreboardService.finishMatch("", ""));
+		
 	}
 
 	@Test
 	public void finishMatch_removeWithNullParameters_ShouldNotThrowExceptionAndDoNotRemoveTheMatch() {
 		scoreboardService.startMatch("Brazil", "Argentina");
 		scoreboardService.startMatch("Spain", "Portugal");
-		scoreboardService.finishMatch(null, null);
+		
 		assertEquals(2, scoreboardService.getLiveMatches().size());
+		assertThrows(IllegalArgumentException.class, () -> scoreboardService.finishMatch(null, null));
 	}
 
 	/* Tests for updateScore */
@@ -144,6 +151,22 @@ public class ScoreboardServiceTest {
 		assertEquals(0, liveMatch.get().getAwayTeamScore());
 
 	}
+	
+	@Test
+	public void updateScore_InvalidMultipleUpdates_ShouldNotUpdateAnything() {
+		scoreboardService.startMatch("Brazil", "Argentina");
+		scoreboardService.updateScore("Sweden", "Germany", 1, 2);
+	
+		Predicate<Match> isPresent = match -> ScoreboardService.checkLiveMatch("Brazil", "Argentina", match);
+		Optional<Match> liveMatch = scoreboardService.getLiveMatches().stream().filter(isPresent).findFirst();
+
+		assertEquals(1, scoreboardService.getLiveMatches().size());
+		assertEquals(0, liveMatch.get().getHomeTeamScore());
+		assertEquals(0, liveMatch.get().getAwayTeamScore());
+		assertThrows(IllegalArgumentException.class, () -> scoreboardService.updateScore(null, null, 1, 2));
+		assertThrows(IllegalArgumentException.class, () -> scoreboardService.updateScore("", "", 1, 2));
+
+	}
 
 	@Test
 	public void updateScore_ValidTeamsWithMultipleUpdates_ShouldUpdateWithLatestScores() {
@@ -163,27 +186,82 @@ public class ScoreboardServiceTest {
 
 	/* Tests for ScoreBoard Summary */
 	@Test
-	public void getLiveMatchScores_ShouldShowSummaryOfLiveMatchesSorted() {
+	public void getLiveMatchScores_StraightForwardStartAndUpdate_ShouldShowSummaryOfLiveMatchesSorted() {
 
+		testSetup_StraightForwardStartUpdate();
+
+		List<Match> expectedLiveMatches = scoreboardService.getLiveMatches();
+		
+		List<Match> actualLiveMatches = scoreboardService.getLiveMatchesSummary();
+		Match actualFirstMatch = actualLiveMatches.stream().findFirst().get();	
+		assertGetLiveMatchesStraightForwardStartUpdate(expectedLiveMatches, actualLiveMatches, actualFirstMatch);
+	}
+
+	public void assertGetLiveMatchesStraightForwardStartUpdate(List<Match> expectedLiveMatches,
+			List<Match> actualLiveMatches, Match actualFirstMatch) {
+		assertEquals(5, actualLiveMatches.size());
+		assertNotNull(expectedLiveMatches);
+		assertTrue(expectedLiveMatches.containsAll(actualLiveMatches));
+		assertTrue(actualLiveMatches.containsAll(expectedLiveMatches));
+		assertEquals("Germany", actualFirstMatch.getHomeTeam());
+		assertEquals("Poland", actualFirstMatch.getAwayTeam());
+		assertEquals(5, actualFirstMatch.getHomeTeamScore());
+		assertEquals(5, actualFirstMatch.getAwayTeamScore());
+	}
+
+	@Test
+	public void getLiveMatchScores_MultipleStartUpdateFinish_ShouldShowSummaryOfLiveMatchesSorted() {
+
+		testSetup_MultipleStartUpdateFinish();
+
+		List<Match> expectedLiveMatches = scoreboardService.getLiveMatches();
+		List<Match> actualLiveMatches = scoreboardService.getLiveMatchesSummary();
+
+		Match actualFirstMatch = actualLiveMatches.stream().findFirst().get();
+		
+		assertGetLiveMatchesForMultipleStartUpdateFinish(expectedLiveMatches, actualLiveMatches, actualFirstMatch);
+	}
+
+	public void assertGetLiveMatchesForMultipleStartUpdateFinish(List<Match> expectedLiveMatches,
+			List<Match> actualLiveMatches, Match actualFirstMatch) {
+		assertEquals(2, actualLiveMatches.size());
+		assertNotNull(expectedLiveMatches);
+		assertTrue(expectedLiveMatches.containsAll(actualLiveMatches));
+		assertTrue(actualLiveMatches.containsAll(expectedLiveMatches));
+		assertEquals("Brazil", actualFirstMatch.getHomeTeam());
+		assertEquals("Argentina", actualFirstMatch.getAwayTeam());
+		assertEquals(4, actualFirstMatch.getHomeTeamScore());
+		assertEquals(5, actualFirstMatch.getAwayTeamScore());
+	}
+	
+	
+	
+	public void testSetup_StraightForwardStartUpdate() {
 		scoreboardService.startMatch("Spain", "Portugal");
 		scoreboardService.updateScore("Spain", "Portugal", 7, 3);
 		scoreboardService.startMatch("Germany", "Poland");
 		scoreboardService.updateScore("Germany", "Poland", 5, 5);
 		scoreboardService.startMatch("Belgium", "France");
 		scoreboardService.updateScore("Belgium", "France", 4, 4);
+		scoreboardService.startMatch("Italy", "Croatia");
+		scoreboardService.updateScore("Italy", "Croatia", 1, 1);
+		scoreboardService.startMatch("Morocco", "Sweden");
+		scoreboardService.updateScore("Morocco", "Sweden", 1, 2);
+	}
 
-		List<Match> expectedLiveMatches = scoreboardService.getLiveMatches();
-		List<Match> liveMatches = scoreboardService.getLiveMatchesSummary();
-
-		Match firstMatch = liveMatches.stream().findFirst().get();
-		assertEquals(3, liveMatches.size());
-		assertNotNull(expectedLiveMatches);
-		assertTrue(expectedLiveMatches.containsAll(liveMatches));
-		assertTrue(liveMatches.containsAll(expectedLiveMatches));
-		assertEquals("Germany", firstMatch.getHomeTeam());
-		assertEquals("Poland", firstMatch.getAwayTeam());
-		assertEquals(5, firstMatch.getHomeTeamScore());
-		assertEquals(5, firstMatch.getAwayTeamScore());
+	public void testSetup_MultipleStartUpdateFinish() {
+		scoreboardService.startMatch("Brazil", "Argentina");
+		scoreboardService.updateScore("Brazil", "Argentina", 1, 2);
+		scoreboardService.updateScore("Brazil", "Argentina", 2, 4);
+		scoreboardService.updateScore("Brazil", "Argentina", 4, 5);
+		scoreboardService.startMatch("Spain", "Portugal");
+		scoreboardService.updateScore("Spain", "Portugal", 7, 3);
+		scoreboardService.finishMatch("Spain", "Portugal");
+		scoreboardService.startMatch("Germany", "Poland");
+		scoreboardService.updateScore("Germany", "Poland", 5, 5);
+		scoreboardService.finishMatch("Germany", "Poland");
+		scoreboardService.startMatch("Belgium", "France");
+		scoreboardService.updateScore("Belgium", "France", 4, 4);
 	}
 
 }
